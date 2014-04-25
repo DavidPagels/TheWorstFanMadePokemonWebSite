9a3d163851f3acbec35662857de6be69130e055c
DavidPagels <pagel093@morris.umn.edu>
2014-04-24 23:34:18 -0500

    finished creating learnset table

--- PokemonGrabber.js
+++ PokemonGrabber.js
@@ -5,8 +5,9 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
+var movesArr = Array();
 
-var PokeArray = ["bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard", "squirtle", "wartortle", "blastoise", "caterpie", "metapod", "butterfree", "weedle", "kakuna", "beedrill", "pidgey", "pidgeotto", "pidgeot", "rattata", "raticate", "spearow", "fearow", "ekans", "arbok", "pikachu", "raichu", "sandshrew", "sandslash", "nidoran-f", "nidorina", "nidoqueen", "nidoran-m", "nidorino", "nidoking", "clefairy", "clefable", "vulpix", "ninetales", "jigglypuff", "wigglytuff", "zubat", "golbat", "oddish", "gloom", "vileplume", "paras", "parasect", "venonat", "venomoth", "diglett", "dugtrio", "meowth", "persian", "psyduck", "golduck", "mankey", "primeape", "growlithe", "arcanine", "poliwag", "poliwhirl", "poliwrath", "abra", "kadabra", "alakazam", "machop", "machoke", "machamp", "bellsprout", "weepinbell", "victreebel", "tentacool", "tentacruel", "geodude", "graveler", "golem", "ponyta", "rapidash", "slowpoke", "slowbro", "magnemite", "magneton", "farfetchd", "doduo", "dodrio", "seel", "dewgong", "grimer", "muk", "shellder", "cloyster", "gastly", "haunter", "gengar", "onix", "drowzee", "hypno", "krabby", "kingler", "voltorb", "electrode", "exeggcute", "exeggutor", "cubone", "marowak", "hitmonlee", "hitmonchan", "lickitung", "koffing", "weezing", "rhyhorn", "rhydon", "chansey", "tangela", "kangaskhan", "horsea", "seadra", "goldeen", "seaking", "staryu", "starmie", "mr. mime", "scyther", "jynx", "electabuzz", "magmar", "pinsir", "tauros", "magikarp", "gyarados", "lapras", "ditto", "eevee", "vaporeon", "jolteon", "flareon", "porygon", "omanyte", "omastar", "kabuto", "kabutops", "aerodactyl", "snorlax", "articuno", "zapdos", "moltres", "dratini", "dragonair", "dragonite", "mewtwo", "mew"];
+var PokeArray = ["bulbasaur", "ivysaur", "venusaur", "charmander", "charmeleon", "charizard", "squirtle", "wartortle", "blastoise", "caterpie", "metapod", "butterfree", "weedle", "kakuna", "beedrill", "pidgey", "pidgeotto", "pidgeot", "rattata", "raticate", "spearow", "fearow", "ekans", "arbok", "pikachu", "raichu", "sandshrew", "sandslash", "nidoran♀", "nidorina", "nidoqueen", "nidoran♂", "nidorino", "nidoking", "clefairy", "clefable", "vulpix", "ninetales", "jigglypuff", "wigglytuff", "zubat", "golbat", "oddish", "gloom", "vileplume", "paras", "parasect", "venonat", "venomoth", "diglett", "dugtrio", "meowth", "persian", "psyduck", "golduck", "mankey", "primeape", "growlithe", "arcanine", "poliwag", "poliwhirl", "poliwrath", "abra", "kadabra", "alakazam", "machop", "machoke", "machamp", "bellsprout", "weepinbell", "victreebel", "tentacool", "tentacruel", "geodude", "graveler", "golem", "ponyta", "rapidash", "slowpoke", "slowbro", "magnemite", "magneton", "farfetch%27d", "doduo", "dodrio", "seel", "dewgong", "grimer", "muk", "shellder", "cloyster", "gastly", "haunter", "gengar", "onix", "drowzee", "hypno", "krabby", "kingler", "voltorb", "electrode", "exeggcute", "exeggutor", "cubone", "marowak", "hitmonlee", "hitmonchan", "lickitung", "koffing", "weezing", "rhyhorn", "rhydon", "chansey", "tangela", "kangaskhan", "horsea", "seadra", "goldeen", "seaking", "staryu", "starmie", "mr._Mime", "scyther", "jynx", "electabuzz", "magmar", "pinsir", "tauros", "magikarp", "gyarados", "lapras", "ditto", "eevee", "vaporeon", "jolteon", "flareon", "porygon", "omanyte", "omastar", "kabuto", "kabutops", "aerodactyl", "snorlax", "articuno", "zapdos", "moltres", "dratini", "dragonair", "dragonite", "mewtwo", "mew"];
 
 var connection = mysql.createConnection({
     user: "Pagels",
@@ -35,48 +36,31 @@ app.get('/', function(req, res){
                 var rightthings = 0;
                 //console.log($('[style$="display:none"]'))
                 var stat = $('[style$="border-collapse:collapse;"]').children().children('[style$="background:#FFFFFF; border:1px solid #D8D8D8;"]').each(function ()
-                {
-                    if((rightthings + 4) % 5 == 0){
-                        var that = this
-                        //setTimeout(function(){
-                                var pattern = /\s(.*)/;
-                                var other = $(that).first().first().text().match(pattern)[1]
-                                if(other == 'PoisonPowder')
-                                    other = 'Poison Powder'
-                                if(other == 'SolarBeam')
-                                    other = 'Solar Beam'
-                                console.log(other)
-                                connection.query('UPDATE pokemonMoves SET moveID=(SELECT id FROM moves WHERE name="' + other + '") WHERE tempIndex=' + overallIndex);
-                                overallIndex++;
-
-
-                        //}, 125 * rightthings * (index + 1))
-                    }
-
-                    rightthings++;
-
-
+               {
+                   var other = $(this).children('a').first().text()
+                   //console.log(other);
+                   if(other == 'Y'){
+                       console.log(index + 1);
+                   }
+//                    if((rightthings + 4) % 5 == 0){
                 })
-//                    var pattern = /\s([^*\n%]+)%*[\n]*|[*]/;
-//                    var other = stat.match(pattern)[1]
-                //console.log(stat);
-//                    if(other == '—' || other == undefined)
-//                        connection.query('UPDATE moves SET accuracy=' + 1337 + ' WHERE id=' + i)
-//                    else
-//                        connection.query('UPDATE moves SET accuracy=' + other + ' WHERE id=' + i)
-//                    i++;
-//                    if(i==2)
-//                        clearInterval(loop);
-                // }, 125)
+                index++;
+                if(index == 152){
+                    doneLoop()
+                    clearInterval(theLoop);
+                }
             }
+            //}, 1500);
         })
-        index++;
-    }, 1000)
-    if(index == 152)
-        window.clearInterval(theLoop);
+    }, 750)
 
-    //}, 1500);
 })
+
+function doneLoop(){
+    for(var i=1; i < 152; i++){
+        movesArr[i].forEach(function(level){ console.log(i + " " + level)})
+    }
+}
 
 app.listen('3000')
 console.log('Magic happens on port 3000');
